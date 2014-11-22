c5e441990b5706d19c1b16835c2b16bd16729ca4
Allan Xiao <allan.xiao@gmail.com>
2014-11-22 15:12:40 -0500

    Add Dialog Window module

---
+++ Innouvous.Utils.DialogWindow/DialogWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Innouvous.Utils
+{
+    /// <summary>
+    /// Adds functionality to disable the X button on the Window
+    /// </summary>
+    public class DialogWindow : Window
+    {
+        private bool shouldClose = false;
+
+        protected void CanClose()
+        {
+            shouldClose = true;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = !shouldClose;
+            
+            base.OnClosing(e);
+        }
+    }
+}
